6c4d8a1a3c08568d90fc243462694c076f4e5d59
Chaunceyo <chaunceyogletree725@gmail.com>
2025-02-28 12:27:36 -0500

    wip

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import NavBar from './pages/NavBar';
-
-
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+import About from './pages/About';
 
 
 function App() {
@@ -15,11 +17,10 @@ function App() {
       <NavBar />
       <nav className='App-nav'>
         <Routes>
-          <Route path='/' element={<Home />}></Route>
-          <Route path='/playstation' element={<PlayStation />}></Route>
-          <Route path='/xbox' element={<Xbox />}></Route>
-          <Route path={`/item/:gameName`} element={<ItemPage />} />
-          <Route path="/callback" element={<CallbackPage />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/about' element={<About />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </nav>
     </div>
---
+++ my-portfolio/src/pages/Projects.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Projects = () => {
+    return(
+        <div>
+          Hello
+        </div>
+    )
+}
+export default Projects;
