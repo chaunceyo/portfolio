78e52bd9c73393b22e0077aaec7b804c347951e6
Chaunceyo <chaunceyogletree725@gmail.com>
2025-02-28 11:30:14 -0500

    wip

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import NavBar from './pages/NavBar';
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+
+    <div className="App">
+      <NavBar />
+      <nav className='App-nav'>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/playstation' element={<PlayStation />}></Route>
+          <Route path='/xbox' element={<Xbox />}></Route>
+          <Route path={`/item/:gameName`} element={<ItemPage />} />
+          <Route path="/callback" element={<CallbackPage />} />
+        </Routes>
+      </nav>
+    </div>
+
+  );
 }
 
-export default App
+export default App;
