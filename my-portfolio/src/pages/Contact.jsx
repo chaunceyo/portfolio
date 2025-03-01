b20e197c7255a174ee9c18ad2036417f74032b7b
Chaunceyo <chaunceyogletree725@gmail.com>
2025-03-01 08:07:07 -0500

    wip

--- my-portfolio/src/pages/Contact.jsx
+++ my-portfolio/src/pages/Contact.jsx
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
+import emailjs from "@emailjs/browser";
 import './Contact.css'
 
 const Contact = () => {
+      const [project, setProject] = useState("");
+      const [name, setName] = useState("");
+      const [email, setEmail] = useState("");
+      const [message, setMessage] = useState("");
+
+      const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (!project || !name || !email || !message) {
+          alert("Please fill out all fields");
+          return;
+        }
+    
+        console.log("Form submitted:", {
+          name,
+          email,
+          project,
+          message,
+        })
+        
+        emailjs
+        .send(
+          "service_rfcurq6", // Replace with your service ID
+          "template_8do67hf", // Replace with your template ID
+          templateParams,
+          process.env.REACT_APP_EMAILJS_PUBLIC_KEY // Replace with your Public Key
+        )
+        .then(() => {
+          alert("Message sent successfully ✅");
+          setProjectType("");
+          setName("");
+          setEmail("");
+          setMessage("");
+        })
+        .catch((err) => console.error("Email Error:", err));
+    };
+      
+    
     return(
        <>
        <div>
@@ -12,19 +51,46 @@ const Contact = () => {
             Fill <br /> out the form below or reach out directly — I will reply within 24 hours.
         </h3>
       </div>
-      <form>
-        <input type="text" placeholder="Name" />
-        <input type="email" placeholder="Email" />
-        <select name="project" id="project">
-          <option value="" selected disabled>Project Type</option>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          placeholder="Name" 
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+
+        <input 
+          type="email" 
+          placeholder="Email" 
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+
+        <select 
+          name="project" 
+          value={project}
+          onChange={(e) => setProject(e.target.value)}
+          required
+        >
+          <option value="" disabled>Project Type</option>
           <option value="new-website">New Website</option>
           <option value="website-redesign">Website Redesign</option>
           <option value="bug-fixes">Bug Fixes</option>
           <option value="seo-services">SEO Services</option>
           <option value="maintenance-plan">Monthly Maintenance Plan</option>
         </select>
-        <textarea placeholder="Tell me about your project"></textarea>
+
+        <textarea 
+          placeholder="Tell me about your project"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          required
+        ></textarea>
+
         <button type="submit">Send Message</button>
+
       </form>
 
      
