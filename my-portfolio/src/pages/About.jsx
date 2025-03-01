f0ab1da40e368f72452835cd480ae399691236e1
Chaunceyo <chaunceyogletree725@gmail.com>
2025-02-28 19:26:48 -0500

    wip

--- my-portfolio/src/pages/About.jsx
+++ my-portfolio/src/pages/About.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import './About.css'
 
 const About = () => {
     return(
-        <div>
-          My name is Chauncey Ogletree, and I am a web developer with experience in Javascript, 
-          CSS, React.js, Node.js, and SQL. With a background in building responsive and user-friendly websites
-        </div>
+        <>
+          <h1 className='about'>About Me</h1>
+          <div className='about-section-1'>
+          <img className='myImg2' src='./myImage2.png'></img>
+            <h5 className='about-info'>
+              👋 Hi, I'm Chauncey Ogletree, a Full Stack Web Developer and graduate <br />
+              of BloomTech's Full Stack Web Development program. I specialize in building <br />
+              modern, responsive websites and web applications using HTML, CSS, JavaScript, <br />
+              React, and Node.js. I'm passionate about turning ideas into functional, user-friendly <br />
+              websites that help businesses grow. Whether it's creating sleek landing pages <br />
+              or full e-commerce platforms, I'm always focused on delivering clean code and <br />
+              great user experiences. <br /> <br />
+              Let's build something amazing together! 🚀
+            </h5>
+          </div>
+          <div>
+            
+          </div>
+        </>
     )
 }
 export default About;
