e7900adc691eb4b2ce6aeb316d722245b765e536
Chaunceyo <chaunceyogletree725@gmail.com>
2025-03-02 20:19:52 -0500

    contact

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 import { createRequire } from 'module';
+import react from '@vitejs/plugin-react';
 const require = createRequire(import.meta.url);
 
 export default defineConfig({
+  plugins: [react()],
+  base: '/',
   resolve: {
     alias: {
       crypto: require.resolve('crypto-browserify')
