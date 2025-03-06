56368f58fc69bdd1db8591b24ee0eef8ed3617bd
Chaunceyo <chaunceyogletree725@gmail.com>
2025-03-06 06:41:26 -0500

    seo testing

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import { createRequire } from 'module';
+import { createHtmlPlugin } from 'vite-plugin-html';
 import react from '@vitejs/plugin-react';
 const require = createRequire(import.meta.url);
 
@@ -8,7 +9,18 @@ export default defineConfig({
     host: true, // This makes the server visible on your local network
     port: 5173, // Default Vite port
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      minify: true,
+      inject: {
+        data: {
+          title: 'Web Dev Chauncey | Custom Websites',
+          description: 'Helping small businesses build custom websites that attract clients and boost online presence.',
+        }
+      }
+    })
+  ],
   base: '/',
   resolve: {
     alias: {
